2fb1e5f4f74206f5b3c9737fb0324dc11e85b87a
Raeva Sayed <raevasayed@10-20-56-114.dynapool.wireless.nyu.edu>
2019-04-15 22:58:13 -0400

    Adding Assignment8

---
+++ Assignment8/assignment8.js
@@ -0,0 +1,34 @@
+
+let ground;
+let box;
+let rock;
+let world
+let engine;
+//let mouseConstraint;
+
+
+function setup() {
+    createCanvas(600,400);
+    engine = Matter.Engine.create();
+    world = engine.world;
+    ground = new Ground(width/2,height-10,width,20);
+    box = new Box(450,300,50,75);
+    rock = new Rock(50, 300,25);
+
+    /*const mouse = Matter.Mouse.create(canvas.elt);
+    const options = {
+        mouse: mouse
+    }
+    mouseConstraint = Matter.MouseConstraint.create(engine, options);
+    Matter.World.add(world,mouseConstraint);
+    */
+
+}
+
+function draw() {
+    background(150);
+    Matter.Engine.update(engine);
+    box.show();
+    ground.show();
+    rock.show();
+}
