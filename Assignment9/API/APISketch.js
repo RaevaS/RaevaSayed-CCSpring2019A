3e9e22749b943420eaeb9e12128e8942781eead5
Raeva Sayed <raevasayed@10-18-39-59.dynapool.wireless.nyu.edu>
2019-04-24 21:46:33 -0400

    commented code

--- Assignment9/API/APISketch.js
+++ Assignment9/API/APISketch.js
@@ -17,21 +17,24 @@ function setup(){
     //canvas paramters
     createCanvas(800, 600);
     background(47,179,169);
+    //Title Text
     textSize(40);
     textAlign(CENTER);
     text("All Live Earthquakes",width/2,40)
+    
+    //for loop to go through all the results
 
     for (var i = 0; i < results.features.length; i++){
+        //variables to store: longitiude, latitude, mapped x, mapped y
         var longitude = results.features[i].geometry.coordinates[0]
         var latitude = results.features[i].geometry.coordinates[1]
         var x = map(longitude,-180,180,0,width)
         var y = map(latitude,-90,90,0,height)
+        //variable to store the magnitude of the earthquakes
         var mag = results.features[i].properties.mag
         fill(179,26,18);
-        ellipse(x,y,20*mag);
+        //multiple radius by magnitude to show scale of earthquake
+        ellipse(x,y,30*mag);
     } 
     
-}
-
-function draw(){
 }
